80c9f5a7d36fa06d8bd3375a5a5d23a0ccc0cdc4
YasudaYasutomo <58838641+YasudaYasutomo@users.noreply.github.com>
2022-09-07 23:20:31 +0200

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -5,7 +5,6 @@
         $name = $_POST["name"];
         $furigana = $_POST["furigana"];
         $email = $_POST["email"];
-        $tel = $_POST["tel"];
         $sex = $_POST["sex"];
         $item = $_POST["item"];
         $content  = $_POST["content"];
@@ -22,14 +21,14 @@
         //mb_send_mail("kanda.it.school.trial@gmail.com", "メール送信テスト", "メール本文");
 
             // 件名を変数subjectに格納
-            $subject = "［自動送信］お問い合わせ内容の確認";
+            $subject = "［自動送信］内容の確認";
 
             // メール本文を変数bodyに格納
         $body = <<< EOM
 {$name}　様
 
-お問い合わせありがとうございます。
-以下のお問い合わせ内容を、メールにて確認させていただきました。
+ありがとうございます。
+以下の内容を、たぬき達に伝えます!!
 
 ===================================================
 【 お名前 】 
@@ -41,10 +40,7 @@
 【 メール 】 
 {$email}
 
-【 電話番号 】 
-{$tel}
-
-【 性別 】 
+【 たぬき 】 
 {$sex}
 
 【 項目 】 
@@ -78,7 +74,7 @@ EOM;
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
-<title>お問い合わせフォーム</title>
+<title>感想フォーム</title>
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -94,7 +90,7 @@ EOM;
             <input type="hidden" name="item" value="<?php echo $item; ?>">
             <input type="hidden" name="content" value="<?php echo $content; ?>">
             <h1 class="contact-title">お問い合わせ 内容確認</h1>
-            <p>お問い合わせ内容はこちらで宜しいでしょうか？<br>よろしければ「送信する」ボタンを押して下さい。</p>
+            <p>内容はこちらで宜しいでしょうか？<br>よろしければ「送信する」ボタンを押して下さい。</p>
             <div>
                 <div>
                     <label>お名前</label>
@@ -109,19 +105,15 @@ EOM;
                     <p><?php echo $email; ?></p>
                 </div>
                 <div>
-                    <label>電話番号</label>
-                    <p><?php echo $tel; ?></p>
-                </div>
-                <div>
-                    <label>性別</label>
+                    <label>たぬき</label>
                     <p><?php echo $sex ?></p>
                 </div>
                 <div>
-                    <label>お問い合わせ項目</label>
+                    <label>項目</label>
                     <p><?php echo $item; ?></p>
                 </div>
                 <div>
-                    <label>お問い合わせ内容</label>
+                    <label>内容</label>
                     <p><?php echo nl2br($content); ?></p>
                 </div>
             </div>
